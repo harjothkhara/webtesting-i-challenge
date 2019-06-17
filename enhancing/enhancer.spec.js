f2e3f14b1b71865df3ea0ed1600e7ece7fb6d2d5
harjothkhara <harjoth.khara@gmail.com>
2019-06-17 12:47:20 -0700

    repair function and repair function test complete

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,40 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+const item1 = {
+    name: 'item 1',
+    enhancement: 12,
+    durability: 50
+};
+
+const item2 = {
+    name: 'item 2',
+    enhancement: 18,
+    durability: 70
+};
+
+const item3 = {
+    name: 'item 3',
+    enhancement: 4,
+    durability: 100
+};
+
+
+
+describe('enhancer.js', () => {
+    describe ('repair() method', () => {
+        it('accepts an item object and returns a new item with durability restored to 100', () => {
+            // setup
+            const expected = { // what we want happen
+                name: item1.name,
+                enhancement: item1.enhancement,
+                durability: 100
+            };
+            // run system under test
+            const result = enhancer.repair(item1);
+            // verify it works
+            expect(result).toEqual(expected)
+            // .toEqual will check properties of object not the reference in memory. 
+        })
+    })
+})
